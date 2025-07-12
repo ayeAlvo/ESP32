4156316833abe3e9c15223ba25194700b6ebf6d0
ayeAlvo <81052206+ayeAlvo@users.noreply.github.com>
2025-07-11 23:51:54 -0300

    Create kafkar_logo.h

---
+++ include/kafkar_logo.h
@@ -0,0 +1,8 @@
+#ifndef KAFKAR_LOGO_H
+#define KAFKAR_LOGO_H
+
+#include <Arduino.h>  // para PROGMEM, si usás Arduino framework
+
+extern const unsigned char bitmap_kafkar_logo[] PROGMEM;
+
+#endif
